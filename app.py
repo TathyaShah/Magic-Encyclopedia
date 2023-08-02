28a67d0bd9e94db2fa83250b4c537a84a99ba3a0
TathyaShah1103 <tathya.s@ahduni.edu.in>
2023-08-02 23:36:28 +0530

    updated login/signup

--- app.py
+++ app.py
@@ -16,9 +16,6 @@ def index():
         # If the user is logged in, render the personalized home page
         return redirect('/')
     return render_template('index.html')
-def redirect_after_2_seconds():
-    time.sleep(2)
-    return redirect(url_for("index"))
 
 @app.route('/alphabets')
 def alphabets():
@@ -42,7 +39,8 @@ def login():
             return render_template('index.html', username=username)
         else:
             # If the user is not found, display "Invalid details" message
-            return f"Invalid details, please signup if new user"
+            login_message_error="Invalid details, please signup if new user"
+            return render_template('login.html',login_message_error=login_message_error)
     return render_template('login.html')
 
 @app.route('/signup', methods=['GET'])
@@ -57,19 +55,21 @@ def signup():
 
         # Check if the password meets the minimum length requirement
         if len(password) < 6:
-            return f'Password must be at least 6 characters long'
-            return redirect('/signup')
+            signup_message_error='Password must be at least 6 characters long'
+            return render_template('signup.html',signup_message_error=signup_message_error)
         
         # Check if the username already exists in the database
         existing_user = mongo.db.details.find_one({'username': username})
 
         if existing_user:
             # If the username is already registered, redirect to login with a message
-            return "User already exists, please login"
+            existing_user="User already exists, please login"
+            return render_template('signup.html',existing_user=existing_user)
         else:
             # If the username is not registered, create a new account
             mongo.db.details.insert_one({'username': username, 'password': password})
-            return "Account created. Now please log in again"
+            account_created="Account created. Now please log in again"
+            return render_template('signup.html',account_created=account_created)         
     return redirect('/')
 
 
